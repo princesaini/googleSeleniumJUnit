8b65234710106d323c36eb44ae267846e66c4692
princesaini <princesaini97@hotmail.com>
2023-10-20 10:51:54 +0530

    added test case to check google is opening and title is Google

--- src/test/java/GoogleTest.java
+++ src/test/java/GoogleTest.java
@@ -1,13 +1,31 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GoogleTest {
+    ChromeDriver driver;
+
+    @BeforeEach
+    public void initializeWebDriver() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
     @Test
-    @DisplayName("This is a Sample Test")
-    public void sampleTest() {
-        String name = "prince";
-        assertEquals("prince", name, "Sample Message");
+    @DisplayName("Checking Google Homepage")
+    public void checkTitle() {
+        driver.get("https://www.google.com");
+        String expected = "Google";
+        String actual = driver.getTitle();
+        assertEquals(expected, actual);
+    }
+
+    @AfterEach
+    public void quitWebDriver() {
+        driver.quit();
     }
 }
