38f4e2028f5561616c3e16c5e7cba789d1d123b4
princesaini <princesaini97@hotmail.com>
2023-10-21 18:38:46 +0530

    added test case to type a query in search box and check title of results page

--- src/test/java/GoogleTest.java
+++ src/test/java/GoogleTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GoogleTest {
@@ -20,6 +22,19 @@ public class GoogleTest {
     public void checkTitle() {
         driver.get("https://www.google.com");
         String expected = "Google";
+        String actual = driver.getTitle();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("searching selenium and then cehcking page title")
+    public void searchQuery() {
+        driver.get("https://www.google.com");
+
+        String query = "selenium";
+        driver.findElement(By.id("APjFqb")).sendKeys(query, Keys.ENTER);
+
+        String expected = query + " - Google Search";
         String actual = driver.getTitle();
         assertEquals(expected, actual);
     }
